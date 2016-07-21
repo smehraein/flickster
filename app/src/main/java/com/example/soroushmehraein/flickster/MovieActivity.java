0ae73460ae3f64860a520d75459ba2700beaa134
Soroush Mehraein <soroush@uber.com>
2016-07-21 01:32:33 -0700

    Initial commit. Receiving basic data and converting to Movie objects.

---
+++ app/src/main/java/com/example/soroushmehraein/flickster/MovieActivity.java
@@ -0,0 +1,52 @@
+package com.example.soroushmehraein.flickster;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.soroushmehraein.flickster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MovieActivity extends AppCompatActivity {
+
+    ArrayList<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray movieJSONResults;
+
+                try {
+                    movieJSONResults = response.getJSONArray("results");
+                    movies = Movie.fromJSONArray(movieJSONResults);
+                    Log.d("DEBUG", movieJSONResults.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+}
